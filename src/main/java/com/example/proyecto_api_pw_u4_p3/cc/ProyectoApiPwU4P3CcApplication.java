d6b42c6bbd8c1363191b486c26d78dbfbef723d4
Jimmy Ortega <fabricioortega2011@gmail.com>
2023-02-22 19:51:06 -0500

    Taller33

---
+++ src/main/java/com/example/proyecto_api_pw_u4_p3/cc/ProyectoApiPwU4P3CcApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_api_pw_u4_p3.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiPwU4P3CcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiPwU4P3CcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyecto_api_pw_u4_p3/cc/ProyectoApiPwU4P3CcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_api_pw_u4_p3.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiPwU4P3CcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
